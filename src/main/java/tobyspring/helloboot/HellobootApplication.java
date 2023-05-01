fce4d79d72a37c5c8a85c1f95044dec10004e785
youhobin <ghdb132@naver.com>
2023-05-01 21:14:09 +0900

    SpringApplication의 모습

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,28 +1,13 @@
 package tobyspring.helloboot;
 
-import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @ComponentScan
@@ -39,27 +24,7 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너 만들기
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() { // 애노테이션으로 변경
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//				dispatcherServlet.setApplicationContext(this); // 스프링 컨테이너를 지정해줌
-
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-						.addMapping("/*"); //서블릿을 등록하고 매핑을 추가, 프론트컨트롤러에서 모든 매핑추가
-				}); // 파라미터로 서블릿 등록 -> 익명클래스를 람다식으로.
-
-				webServer.start(); // 톰캣 서블릿 컨테이너가 동작
-			}
-		};
-		applicationContext.register(HellobootApplication.class);
-		applicationContext.refresh(); // 빈 만들어줌.
-
-
+		MySpringApplication.run(HellobootApplication.class, args);
 	}
 
 }
---
+++ src/main/java/tobyspring/helloboot/MySpringApplication.java
@@ -0,0 +1,30 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MySpringApplication {
+    public static void run(Class<?> applicationClass, String[]... args) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() { // 애노테이션으로 변경
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//				dispatcherServlet.setApplicationContext(this); // 스프링 컨테이너를 지정해줌
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                        .addMapping("/*"); //서블릿을 등록하고 매핑을 추가, 프론트컨트롤러에서 모든 매핑추가
+                }); // 파라미터로 서블릿 등록 -> 익명클래스를 람다식으로.
+
+                webServer.start(); // 톰캣 서블릿 컨테이너가 동작
+            }
+        };
+        applicationContext.register(applicationClass);
+        applicationContext.refresh(); // 빈 만들어줌.
+    }
+}
