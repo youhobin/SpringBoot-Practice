46f133f84405fc7eaf06d262bfa350dfe959e2f0
youhobin <ghdb132@naver.com>
2023-05-01 14:21:23 +0900

    DispatcherServlet으로 전환
    - 스프링의 DispatcherServlet으로 전환
    - 애노테이션으로 매핑

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,13 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+@RequestMapping // 클래스 정보를 먼저 참고.
 public class HelloController {
     // 스프링 컨테이너에서 주입받도록
     private final HelloService helloService;
@@ -10,6 +16,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
 //        SimpleHelloService helloService = new SimpleHelloService();
 
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -22,34 +24,16 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너 만들기
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class); // 어떤 메타정보로 만들지, 컨트롤러 등록
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh(); // 빈 만들어줌.
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 인증, 보안, 다국어, 공통 기능
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { // hello URL일때
-						String name = req.getParameter("name");
-
-						// 빈을 가져와서 사용
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-
-						//응답 : 상태코드, 헤더 (바디가 어떤 타입인지), 바디 3가지가 필요
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE); //String 값은 오타발생 확률이 있음.
-						resp.getWriter().println(ret);
-
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-			}).addMapping("/*"); //서블릿을 등록하고 매핑을 추가, 프론트컨트롤러에서 모든 매핑추가
+			servletContext.addServlet("dispatcherServlet",
+					new DispatcherServlet(applicationContext) // WebApplicationContext 필요
+				).addMapping("/*"); //서블릿을 등록하고 매핑을 추가, 프론트컨트롤러에서 모든 매핑추가
 
 		}); // 파라미터로 서블릿 등록 -> 익명클래스를 람다식으로.
 
