c8b1fb4412e4a56219c991db27d8e3bed83158af
youhobin <ghdb132@naver.com>
2023-05-01 13:14:43 +0900

    서블릿 컨테이너에 서블릿 등록

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,13 +3,34 @@ package tobyspring.helloboot;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+					//응답 : 상태코드, 헤더 (바디가 어떤 타입인지), 바디 3가지가 필요
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello"); //서블릿을 등록하고 매핑을 추가
+
+		}); // 파라미터로 서블릿 등록 -> 익명클래스를 람다식으로.
+
 		webServer.start(); // 톰캣 서블릿 컨테이너가 동작
 	}
 
