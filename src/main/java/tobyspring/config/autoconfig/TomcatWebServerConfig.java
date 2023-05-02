b308b6d0eef222d6a070eee651766c85170a256d
youhobin <ghdb132@naver.com>
2023-05-02 16:52:26 +0900

    자동 구성에 Environment 프로퍼티 적용
    application.properties 에 값 추가

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 import tobyspring.config.ConditionalMyOnClass;
@@ -14,7 +15,9 @@ import tobyspring.config.MyAutoConfiguration;
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean   // 존재하지 않으면 빈을 등록하도록. 유저 구성 정보 빈은 이미 등록이 되므로.
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(env.getProperty("contextPath")); // /app가 URI에 붙게됨
+        return factory;
     }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,10 +1,24 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication // 만든 SpringBootAnnotation
 public class HellobootApplication {
+
+//	@Bean
+//	ApplicationRunner applicationRunner(Environment env) {
+//		// 프로퍼티스에서 가져옴. 보다 환경변수가 더 우선순위 높음
+//		// 보다 시스템 변수가 더 높음
+//		return args -> {
+//			String name = env.getProperty("my.name");
+//			System.out.println("my.name: " + name);
+//		};
+//	}
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너 만들기
