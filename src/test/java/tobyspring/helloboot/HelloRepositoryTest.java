4f8ad121ea1461419294829bf2b35fa67a1e0c82
youhobin <ghdb132@naver.com>
2023-05-04 10:26:42 +0900

    jdbcTemplate 레포지토리

---
+++ src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -0,0 +1,30 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HellobootTest
+public class HelloRepositoryTest {
+    @Autowired HelloRepository helloRepository;
+    @Autowired JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    public void findHelloFailed() throws Exception {
+        Assertions.assertThat(helloRepository.findHello("hi")).isNull();
+
+    }
+
+    @Test
+    void increaseCount() {
+        helloRepository.increaseCount("hello");
+        Assertions.assertThat(helloRepository.countOf("hello")).isEqualTo(1);
+    }
+}
